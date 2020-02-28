84ddfdb31ff987887ba14704ec041baf5e0dcb03
Matthew Lee <melee7@asu.edu>
2020-02-27 23:39:19 -0700

    Commit with new methods.

--- cse360assign2/src/cse360assign2/SimpleList.java
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -1,10 +1,11 @@
 /**
- * Author 		: Matthew Lee
+ * Author 			: Matthew Lee
  * Class ID 		: 387
  * Assignment # 	: 1
- * Description 	: This file contains the class SimpleList.<!-- --> Further details of the SimpleList class 
- * 				  can be found below.<!-- --> The main idea of this class is to add, remove, and manipulate
- *   			  an array of size 10.	
+ * Description 		: This file contains the class SimpleList.<!-- --> Further details of the SimpleList class 
+ * 				  	can be found below.<!-- --> The main idea of this class is to add, remove, and manipulate
+ *   			  	an array of size 10.	
+ * Github Link  	:  
  */
 
 package cse360assign2;
@@ -40,7 +41,8 @@ public class SimpleList {
 	/** 
 	    * This method will add the parameter integer to the front of the list.<!-- -->
 	    * By adding to the front it will push all other numbers back in the list.<!-- -->
-	    * Parameter for this method is the integer that will be added to the front. 
+	    * Parameter for this method is the integer that will be added to the front.<!-- -->
+	    * Note: if the list is initially full, the size will be increased by 50% 
 	    */
 	public void add(int addToFront) {
 		
@@ -49,13 +51,14 @@ public class SimpleList {
 		if(list[size-1] != 0) {
 			int newSize = (int) Math.floor(1.5 * size); 
 			int[] newList = new int[newSize]; 
-			for(int index = 0; index < newSize; index++) {
+			for(int index = 0; index < size-1; index++) {
 				newList[index] = list[index]; 
 			}
 			list = newList; 
 		}
 		
-		for(int index = 9; index > 0; index--) {
+		int newSize = list.length - 1;
+		for(int index = newSize; index > 0; index--) {
 			if(list[index-1] != 0) {
 				list[index] = list[index-1]; 	
 			}
@@ -69,7 +72,8 @@ public class SimpleList {
 	/** 
 	    * This method will remove the parameter integer in any index of the list.<!-- -->
 	    * By removing the integer, all other contents will be pulled forward.<!-- -->
-	    * Parameter for this method is the integer that is to be removed at any location in array. 
+	    * Parameter for this method is the integer that is to be removed at any location in array.<!-- -->
+	    * Note: if the list is 25% empty then the size of the list will decrease by 25%. 
 	    */
 	public void remove(int removeIfFound) {
 		for(int index = 0; index < 10; index++) {			//first loop will check entire array for input int
@@ -140,5 +144,73 @@ public class SimpleList {
 			}
 		}
 		return searchVar; 
+	}
+	
+	/** 
+	    * Rather then add to the front of the list, this method will append to the back.<!-- --> 
+	    * Note: if the list is initially full, the list will increase in size by 50%. 
+	    */
+	public void append(int value) {
+		int size = list.length; 
+		int position = 0; 
+		
+		if(list[size-1] != 0) {
+			int newSize = (int) Math.floor(1.5 * size); 
+			int[] newList = new int[newSize]; 
+			for(int index = 0; index < newSize; index++) {
+				newList[index] = list[index]; 
+			}
+			list = newList; 
+		}
+		
+		for(int index = 0; index < size; index++) {
+			if(list[index] != 0) {
+				position++; 
+			}
+		}
+		
+		list[position] = value; 
+		
+	}
+	
+	/** 
+	    * This method will return the value of the first element in the list.<!-- --> 
+	    * Note: if there are no elements in the list, then -1 will be returned 
+	    */
+	public int first() {
+		if(list[0] == 0) {
+			return -1; 
+		}
+		
+		return list[0]; 
+	}
+	
+	/** 
+	    * This method will return the value of the last element in the list.<!-- --> 
+	    * Note: if there are no elements in the list, then -1 will be returned 
+	    */
+	public int last() {
+		if(list[0] == 0) {
+			return -1; 
+		}
+		
+		int size = list.length; 
+		int position = 0; 
+		for(int index = 0; index < size; index++) {
+			if(list[index] != 0) {
+				position++; 
+			}
+		}
+		
+		return list[position-1]; 
+	}
+	
+	/** 
+	    * Method to attain the current size of the array (list).<!-- --> 
+	    * This will return the complete size, not just the filled elements. 
+	    */
+	public int size() {
+		int size = list.length; 
+		return size; 
 	}
 }
